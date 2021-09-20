ba1321c3662a289b01c5b2191ffb47f8d586c537
Nataly <natasha.totska@yahoo.com>
2021-09-20 20:04:22 +0300

    Made home work

--- model/index.js
+++ model/index.js
@@ -1,15 +1,58 @@
-// const fs = require('fs/promises')
-// const contacts = require('./contacts.json')
+const { v4 } = require("uuid");
+const fs = require("fs").promises;
+const path = require("path");
 
-const listContacts = async () => {}
+const contactsPath = path.join(__dirname, "contacts.json");
 
-const getContactById = async (contactId) => {}
+const updateContacts = async (newProducts) => {
+  await fs.writeFile(contactsPath, JSON.stringify(newProducts));
+};
 
-const removeContact = async (contactId) => {}
+const listContacts = async () => {
+  const data = await fs.readFile("./model/contacts.json");
+  const contacts = JSON.parse(data);
+  return contacts;
+};
 
-const addContact = async (body) => {}
+const getContactById = async (contactId) => {
+  const contacts = await listContacts();
+  const contact = contacts.find((item) => item.id.toString() === contactId.toString());
+  if (!contact) {
+    return null;
+  }
+  return contact;
+};
 
-const updateContact = async (contactId, body) => {}
+const removeContact = async (contactId) => {
+  const contacts = await listContacts();
+  const idx = contacts.findIndex((item) => item.id === contactId);
+  if (idx === -1) {
+    return null;
+  }
+  const newContacts = contacts.filter((item) => item.id !== contactId);
+  await updateContacts(newContacts);
+  return contactId;
+};
+
+const addContact = async (body) => {
+  const contacts = await listContacts();
+  const newContact = { ...body, id: v4() };
+  const newContacts = [...contacts, newContact];
+  await updateContacts(newContacts);
+  return newContact;
+};
+
+const updateContact = async (contactId, body) => {
+  const contacts = await listContacts();
+  const idx = contacts.findIndex((item) => item.id === contactId);
+  if (idx === -1) {
+    return null;
+  }
+  const updateContact = { ...contacts[idx], ...body };
+  contacts[idx] = updateContact;
+  await updateContacts(contacts);
+  return updateContact;
+};
 
 module.exports = {
   listContacts,
@@ -17,4 +60,4 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
-}
+};
