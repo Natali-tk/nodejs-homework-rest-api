6fb9539f58d84ace5bf3ec4592ea1dc485c80262
Nataly <natasha.totska@yahoo.com>
2021-10-04 14:10:00 +0300

    fix bag

--- controllers/contacts/getAll.js
+++ controllers/contacts/getAll.js
@@ -2,7 +2,6 @@ const { sendSuccessRes } = require("../../helpers");
 const { Contact } = require("../../models");
 
 const getAll = async (req, res) => {
-  const userId=req.user.id
   const result = await Contact.find({}, "_id name email phone favorite");
   sendSuccessRes(res, { result });
 };
